1f9853056548106a50c07caaebee4fbc8dcd596e
shivsingh001 <shivamkumar0346@gmail.com>
2024-01-16 22:59:14 +0530

    Setup the server

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const {PORT } = require('./config/serverConfig');
+
+const app = express();
+
+
+
+
+const prepareAndStartServer = () => {
+    app.listen(PORT, () => {
+    console.log(`Server Started at ${PORT}`);
+    });
+}
+
+
+prepareAndStartServer();
